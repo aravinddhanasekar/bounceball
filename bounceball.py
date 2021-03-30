786378bc5755bd014fc4d8135505e839cc36779f
Hephzi <52066666+hephzibahponcellat@users.noreply.github.com>
2021-03-30 12:30:18 +0530

    code cleanup

--- bounceball.py
+++ bounceball.py
@@ -2,30 +2,20 @@ import pygame
 from bounceballmod import *
 
 
-pygame.init()
-screen = pygame.display.set_mode(size)
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode(size)
 
-coords = generate_coords(num_lines, False)
-game_on = True
+    coords = generate_coords(num_lines, False)
+    game_on = True
 
-while game_on:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			game_on = False
+    while game_on:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_on = False
 
-	screen.fill(pink)
+        draw_paddles(coords, screen, speed)
 
-	for coord in coords:
-		#print(coord[0][1])
-		coord[0][1] -= 0.1
-		coord[1][1] -= 0.1
 
-		if coord[0][1] < 0:
-			coords.remove(coord)
-
-			coords.append(generate_coords(1, True)[0])
-			print(len(coords))
-
-		pygame.draw.aalines(screen, BLACK, False, coord)
-	
-	pygame.display.flip()
+if __name__ == "__main__":
+    main()
--- bounceballmod.py
+++ bounceballmod.py
@@ -1,28 +1,71 @@
 from random import randint
+import pygame
 
-size = [600, 600]
 
-pink = (255, 255, 255)
-BLACK = (0,0,0)
-
+screen_height = 600
+screen_width = 600
 len_line = 80
 num_lines = 7
+speed = 0.1
+start = 0
+size = [screen_width, screen_height]
+
+# colors
+GREEN = (102, 255, 102)
+BLACK = (0, 0, 0)
 
 
 def generate_coords(num, from_bottom):
-	coords = []
+    """
+    coordinates will be generated as below
 
-	for i in range(num_lines):
-		x1 = randint(0, 600 - len_line)
-		x2 = x1+len_line
+    coords = [
+                [[x1,y1],[x2,y2]],
+                [[x1,y1],[x2,y2]],
+                ...
+            ]
 
-		if from_bottom:
-			y1 = y2 = 600
-		else:
-			y1 = randint(0,600)
-			y2 = y1
+    the num of coordinate in the coordinates (plural) list
+    would be equal to the arg num
 
-		coord = [[x1,y1], [x2,y2]]
-		coords.append(coord)
+    if from_bottom is True, then the coordinates y1 and y2
+    would be equal to the screen bottom points ie., screen height
 
-	return coords
+    returns coordinates (plural)
+    """
+    coords = []
+
+    for _ in range(num):
+        x1 = randint(start, screen_width - len_line)
+        x2 = x1+len_line
+
+        if from_bottom:
+            y1 = y2 = screen_height
+        else:
+            y1 = y2 = randint(start, screen_height)
+
+        coord = [[x1, y1], [x2, y2]]
+        coords.append(coord)
+
+    return coords
+
+
+def draw_paddles(coords, screen, speed):
+    """
+    draws paddles in the given screen
+    moves the paddles in the give speed
+
+    paddles are drawn in the given coordinate positions
+    """
+    screen.fill(BLACK)
+
+    for coord in coords:
+        coord[0][1] -= speed
+        coord[1][1] -= speed
+
+        if coord[0][1] < start:
+            coords.remove(coord)
+            coords.append(generate_coords(1, True)[0])
+
+        pygame.draw.aalines(screen, GREEN, False, coord)
+    pygame.display.flip()
